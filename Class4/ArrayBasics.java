98d7d409b83d296175b1418b899f87a2279cdcee
patelaj94 <patelaj94@yahoo.com>
2018-02-17 21:47:34 -0500

    Add files via upload

---
+++ Class4/ArrayBasics.java
@@ -0,0 +1,36 @@
+package Class4;
+import java.util.Arrays;
+
+public class ArrayBasics {
+
+	public static void main(String[] args) {
+		// Initialize an array
+		double[] sampleArray = new double[5];
+		
+		// Adding data to the array
+		sampleArray[0] = 30.0;
+		sampleArray[1] = 35.0;
+		sampleArray[2] = 40.0;
+		sampleArray[3] = 45.0;
+		sampleArray[4] = 50.0;
+		
+		
+
+		System.out.println(sampleArray[1]);
+		System.out.println(sampleArray.length);
+		
+		//System.out.println(sampleArray[5]);
+		//sampleArray[5] = 60.0;
+		
+		
+		//System.out.println(sampleArray[5]);
+	
+	
+		// Array Initializer - Java shorthand notation for initializing arrays
+		double[] duplicateSampleArray = {30.0, 35.0, 40.0, 45.0, 50.0};
+		System.out.println(Arrays.toString(duplicateSampleArray));
+		System.out.println(duplicateSampleArray);
+
+	}
+
+}
---
+++ Class4/ArrayClass.java
@@ -0,0 +1,44 @@
+package Class4;
+import java.util.Arrays;
+
+public class ArrayClass {
+
+	public static void main(String[] args) {
+		// Page 253 in Liang Book - The Arrays Class
+		double[] numbers = {6.3, 4.2, 1.4, 8.5, 9.0, 5.5, 11.0, 10.2};
+		
+		// Binary Search allows us to find the index of the specified value
+		System.out.println("8.5's index in our array is index: " + Arrays.binarySearch(numbers, 8.5));
+		
+		
+		// Sort the array
+		//java.util.Arrays
+		Arrays.sort(numbers);
+		System.out.println(Arrays.toString(numbers));
+		
+		
+		System.out.println("8.5's index in our array is index: " + Arrays.binarySearch(numbers, 8.5));
+		
+
+		
+		// 2-D arrays
+		int[][] matrix = { { 1, 2, 3, 4 },
+                			   { 5, 6, 7, 8 },
+                			   { 9, 10, 11, 12 }
+                			 };
+		
+		System.out.println(matrix[0][1]);
+		System.out.println(matrix[1][2]);
+		System.out.println(matrix[2][1]);
+		
+		
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j]);	
+			}
+		}
+		
+		
+	}
+
+}
